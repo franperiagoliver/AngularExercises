1fb5fd7087e3647be5e8cc020899e71e066b1016
franperiagoliver <franperiagoliver@gmail.com>
2017-09-07 16:51:09 +0200

    First Break

--- art-gallery-shop-app/src/app/app.component.ts
+++ art-gallery-shop-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-art-gallery',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Art Gallery Shop';
+  myArtWork = {
+    'id' : 1,
+    'name' : 'The Guernica',
+    'type' : 'Picture',
+    'medium' : 'Oil on canvas',
+    'description' : 'Guernica is a mural-sized oil painting on canvas by Spanish artist Pablo Picasso completed in June 1937.',
+    'stock' : 1
+  };
 }
